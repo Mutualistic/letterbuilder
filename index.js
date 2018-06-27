5b05e4d31f69d35c6fb885819773f4b14bd05088
Lasse Diercks <github@lassediercks.de>
2018-06-27 10:21:29 +0200

    THE BIG REFACTORING

--- index.js
+++ index.js
@@ -2,71 +2,41 @@ document.addEventListener("DOMContentLoaded", function() {
   let nameInput = document.querySelector("#name");
   let nameTarget = document.querySelector("#nameTarget");
 
-  nameInput.addEventListener("input", function() {
-    updateValue(this.value, nameTarget);
-  });
-
-  if (localStorage.getItem(nameTarget.id)) {
-    updateValue(nameInput.value, nameTarget);
-  }
+  syncValue(nameInput, nameTarget);
 
   let titleInput = document.querySelector("#title");
   let titleTarget = document.querySelector("#titleTarget");
 
-  titleInput.addEventListener("input", function() {
-    updateValue(this.value, titleTarget);
-  });
-
-  if (localStorage.getItem(titleTarget.id)) {
-    updateValue(titleInput.value, titleTarget);
-  }
+  syncValue(titleInput, titleTarget);
 
   let streetInput = document.querySelector("#street");
   let streetTarget = document.querySelector("#streetTarget");
 
-  streetInput.addEventListener("input", function() {
-    updateValue(this.value, streetTarget);
-  });
-
-  if (localStorage.getItem(streetTarget.id)) {
-    updateValue(streetInput.value, streetTarget);
-  }
+  syncValue(streetInput, streetTarget);
 
   let postalcodeInput = document.querySelector("#postalcode");
   let postalcodeTarget = document.querySelector("#postalcodeTarget");
 
-  postalcodeInput.addEventListener("input", function() {
-    updateValue(this.value, postalcodeTarget);
-  });
-
-  if (localStorage.getItem(postalcodeTarget.id)) {
-    updateValue(postalcodeInput.value, postalcodeTarget);
-  }
+  syncValue(postalcodeInput, postalcodeTarget);
 
   let cityInput = document.querySelector("#city");
   let cityTarget = document.querySelector("#cityTarget");
 
-  cityInput.addEventListener("input", function() {
-    updateValue(this.value, cityTarget);
-  });
-
-  if (localStorage.getItem(cityTarget.id)) {
-    updateValue(cityInput.value, cityTarget);
-  }
+  syncValue(cityInput, cityTarget);
 
   let emailInput = document.querySelector("#email");
   let emailTarget = document.querySelector("#emailTarget");
 
-  emailInput.addEventListener("input", function() {
-    updateValue(this.value, emailTarget);
-  });
+  syncValue(emailInput, emailTarget);
 
-  if (localStorage.getItem(emailTarget.id)) {
-    updateValue(emailInput.value, emailTarget);
-  }
+  function syncValue(input, target) {
+    if (localStorage.getItem(target.id)) {
+      target.innerHTML = localStorage.getItem(target.id);
+    }
 
-  function updateValue(val, target) {
-    target.innerHTML = val;
-    localStorage.setItem(target.id, val);
+    input.addEventListener("input", function() {
+      target.innerHTML = input.value;
+      localStorage.setItem(target.id, input.value);
+    });
   }
 });
