cc2f58780c7aff9dfe12bb0eeb2a39ab0e2d5103
Lasse Diercks <github@lassediercks.de>
2018-06-29 10:25:28 +0200

    feat: send post request to get pdf

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const PRINT_SERVICE_URL = "https://printmenow.herokuapp.com";
+const PRINT_SERVICE_URL = "https://printmenow.herokuapp.com/endpoint";
 const BASE_URL = "https://lassediercks.github.io/sistente";
 
 const printButton = document.querySelector("#printbutton");
+
+let urlForPrintservice;
 
 document.addEventListener("DOMContentLoaded", function() {
   const nameInput = document.querySelector("#name");
@@ -46,43 +48,70 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     if (getValueOfParam(input.id)) {
-      target.innerHTML = getValueOfParam(input.id)
-        .split("ASPACE")
-        .join(" ");
+      target.innerHTML = getValueOfParam(input.id);
     }
   }
+
+  let params = {};
+
+  const updatePrintLink = input => {
+    params[input.id] = input.value;
+
+    const toUrlString = (string, keyValue) =>
+      string.concat(`${keyValue[0]}=${keyValue[1]}&`);
+
+    let paramAttach = Object.entries(params).reduce(toUrlString, "");
+    urlForPrintservice = `${BASE_URL}?${paramAttach}`;
+
+    console.log(urlForPrintservice);
+    return urlForPrintservice;
+  };
+
+  function getValueOfParam(name) {
+    let url = window.location.search.substring(1);
+    let vars = url.split("&");
+
+    for (let i = 0; i < vars.length; i++) {
+      let pair = vars[i].split("=");
+
+      if (pair[0] == name) {
+        return pair[1];
+      }
+    }
+  }
+
+  function createPdf(body) {
+    const blob = new Blob([body], { type: "application/pdf" });
+    const data = window.URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+
+    console.log(typeof body);
+    // The actual download
+    // var filename = "blah.pdf";
+    link.href = data;
+    console.log(link);
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  function downloadPdf(url) {
+    console.log(url);
+    let options = {
+      method: "POST",
+      body: JSON.stringify({ url: url }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(PRINT_SERVICE_URL, options)
+      .then(response => response.arrayBuffer())
+      .then(createPdf);
+  }
+
+  printButton.addEventListener("click", () => {
+    if (urlForPrintservice) downloadPdf(urlForPrintservice);
+  });
 });
-
-let params = {};
-
-const updatePrintLink = input => {
-  params[input.id] = input.value;
-
-  const toUrlString = (string, keyValue) =>
-    string.concat(`${keyValue[0]}=${keyValue[1]}&`);
-
-  let paramAttach = Object.entries(params).reduce(toUrlString, "");
-  let urlForPrintservice = encodeURIComponent(
-    `${BASE_URL}?${paramAttach.split(" ").join("ASPACE")}`
-  );
-
-  console.log(urlForPrintservice);
-
-  printButton.setAttribute(
-    "href",
-    `${PRINT_SERVICE_URL}/${urlForPrintservice}`
-  );
-};
-
-function getValueOfParam(name) {
-  let url = window.location.search.substring(1);
-  let vars = url.split("&");
-
-  for (let i = 0; i < vars.length; i++) {
-    let pair = vars[i].split("=");
-
-    if (pair[0] == name) {
-      return pair[1];
-    }
-  }
-}
